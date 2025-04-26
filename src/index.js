cc6056a6ec0eac627f2155e4b0486a4a1b6b0479
vandanafuloria <vandanafuloria02@gmail.com>
2025-04-26 16:47:14 +0530

    latest news rendering on dom completed

--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
 import "./styles.css";
+
+const apiKey = `d3b4aa55576c4658ab70b936ed107197`;
 const primaryLink = document.getElementById("primary-a");
 const primaryTitle = document.getElementById("primary-title");
 const primaryImg = document.getElementById("primary-img");
@@ -10,40 +12,116 @@ const secTitle = document.getElementById("sec-title");
 const secImg = document.getElementById("sec-img");
 const secDesc = document.getElementById("sec-desc");
 
+/************************************************************************** */
+const latestNewsContainerEl = document.querySelector(".latest-news");
+
 function addToDom(news) {
   console.log(news.articles[0]);
   primaryLink.href = news.articles[0].url;
   primaryTitle.textContent = news.articles[0].title;
   primaryImg.src = news.articles[0].urlToImage;
-  console.log(primaryImg);
+
   primaryDesc.textContent = news.articles[0].description;
   /*********************************************************/
 
   secLink.href = news.articles[1].url;
   secTitle.textContent = news.articles[1].title;
   secImg.src = news.articles[1].urlToImage;
-
   secDesc.textContent = news.articles[1].description;
+
+  /************************************************************** */
 }
 
 function getHeadlines() {
   const country = "india";
   const keyword = "science";
-  const x = fetch(
-    `https://newsapi.org/v2/everything?q=${keyword} + ${country}&language=en&sortBy=publishedAt&apiKey=d3b4aa55576c4658ab70b936ed107197`
-  )
+  const url = `https://newsapi.org/v2/everything?q=${keyword} + ${country}&language=en&sortBy=publishedAt&apiKey=${apiKey}`;
+  const x = fetch(url)
     .then((res) => {
       return res.json();
     })
     .then((response) => {
       addToDom(response);
-      console.log(response);
     })
     .catch((err) => {
       console.log("error occured");
     });
 }
 
-function getLatestNews() {}
+function addInformationToSkeleton(latest) {
+  for (let i = 0; i < latest.articles.length; i++) {
+    const newsBox = createSkeleton();
+    // img setting
+    if (!latest.articles[i].urlToImage) continue;
+    const img = newsBox.firstElementChild;
+    img.src = latest.articles[i].urlToImage;
+    if (img.src == null) continue;
+    // link setting
+    const link = newsBox.firstElementChild.nextSibling;
+    link.href = latest.articles[i].url;
+    // title setting;
+    const h1 = link.firstChild;
+    h1.textContent = latest.articles[i].title;
+    // desc setting;
+    const desc = newsBox.firstChild.nextSibling.nextSibling;
+    desc.textContent = latest.articles[i].description;
+    // setting details
+    const details = desc.nextSibling;
+
+    const date = latest.articles[i].publishedAt.split("T");
+    details.firstChild.textContent = date[0];
+
+    details.firstChild.nextSibling.textContent = latest.articles[i].author;
+
+    latestNewsContainerEl.appendChild(newsBox);
+  }
+}
+
+function createSkeleton() {
+  const newsContainer = document.createElement("div");
+  newsContainer.classList.add("news");
+
+  const imgEl = document.createElement("img");
+  /************************************************************** */
+  const anchorEl = document.createElement("a");
+  const headingEl = document.createElement("h1");
+  anchorEl.appendChild(headingEl);
+  /********************************************************** */
+  const desc = document.createElement("p");
+  desc.classList.add("m1");
+
+  /*********************************************************************** */
+  const detailContainer = document.createElement("div");
+  detailContainer.classList.add("details");
+  const p1 = document.createElement("p");
+
+  const p2 = document.createElement("p");
+
+  detailContainer.append(p1, p2);
+
+  /****************************************************** */
+  newsContainer.append(imgEl, anchorEl, desc, detailContainer);
+  console.log("last line");
+
+  return newsContainer;
+}
+
+function getLatestNews() {
+  const category = "general";
+  const urlLatest = `https://newsapi.org/v2/everything?q=india&language=en&sortBy=publishedAt&pageSize=30&apiKey=${apiKey}`;
+  const latestNews = fetch(urlLatest)
+    .then((res) => {
+      console.log(res);
+      return res.json();
+    })
+    .then((response) => {
+      console.log(response);
+      addInformationToSkeleton(response);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+}
+getLatestNews();
 
 getHeadlines();
